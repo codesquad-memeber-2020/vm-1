a929a5ccf76157d428c5fda093cd7baa5a5bf93c
kiyoesjh <hyeyaa125@gmail.com>
2020-03-18 02:30:09 +0900

    [업데이트] vending machine model, select view

--- src/model/VendingMachineModel.js
+++ src/model/VendingMachineModel.js
@@ -7,30 +7,56 @@ class VendingMachineModel extends Observable {
     this.state = {
       accumulatedAmount: 0,
       targetValue: null,
-      //targetValue 를 this.state data로 할당
-      productNumber: "",
-      productName: ""
-      // productNumber 와 productName => Number로 판별하고 Name에 할당해서 view에 넘겨주기 위함
+      productNumber: '',
+      productName: '',
+      type: ''
     };
   }
 
   addEventWallet() {
     const target = event.target;
     const targetCount = Number(
-      target.parentNode.querySelector(".count_index").textContent
-    ); //0개일때 증가되지 않도록 예외처리를 위함
-
-    // console.log(target.nextElementSibling.textContent);
+      target.parentNode.querySelector('.count_index').textContent
+    );
 
     if (!targetCount) return;
 
-    //...누적금액 증가
     this.state.targetValue = target.value;
     this.state.accumulatedAmount += Number(target.value);
-
+    this.state.type = 'wallet';
     this.notify(this.state);
   }
-  addEventDial() {}
+
+  addEventDial(dial, type) {
+    if (type === 'number') this.state.productNumber += dial;
+    else if (type === 'select') this.clickSelectDial(this.state.productNumber);
+    else if (type === 'cancel') this.state.productNumber = '';
+  }
+
+  clickSelectDial (number) {
+    const productId = Number(number);
+    if(!productId || productId > productData.length) return this.state.productNumber = '';
+    this.state.type = 'dial';
+    const filteringProduct = this.filterProduct(productId);
+
+    if (this.state.accumulatedAmount < Number(filteringProduct.price)) {
+      this.state.productNumber = '';
+      this.state.type = 'err';
+      return this.notify(this.state);
+    }
+
+    this.state.productName = filteringProduct.name;
+    this.state.accumulatedAmount -= Number(filteringProduct.price);
+    this.notify(this.state);
+
+    this.state.productNumber = '';
+  }
+
+  filterProduct (id) {
+    const filteringProduct = productData.filter(product => product.id === id)[0];
+
+    return {name: filteringProduct.name, price: filteringProduct.price};
+  }
 }
 
 export default VendingMachineModel;
