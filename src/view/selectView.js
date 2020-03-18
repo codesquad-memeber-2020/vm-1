099847b935fe17b2064e76d3f1ed4cb72987ee72
kiyoesjh <hyeyaa125@gmail.com>
2020-03-19 01:27:35 +0900

    [수정] 코드 정리

--- src/view/selectView.js
+++ src/view/selectView.js
@@ -8,10 +8,10 @@ class SelectView {
   }
 
   bindEvent() {
-    const dial = this.element.querySelector(".choice_dial");
+    const dial = this.element.querySelector('.choice_dial');
 
-    dial.addEventListener("click", e => {
-      if (e.target.tagName !== "BUTTON") return;
+    dial.addEventListener('click', e => {
+      if (e.target.tagName !== 'BUTTON') return;
       const targetValue = e.target.innerText;
       const targetDataType = e.target.dataset.type;
 
@@ -20,7 +20,7 @@ class SelectView {
   }
 
   render({ targetValue, accumulatedAmount, productName, type }) {
-    const stateLogEle = this.element.querySelector(".choice_message");
+    const stateLogEle = this.element.querySelector('.choice_message');
     const logText = this.renderStateLog({ targetValue, productName, type });
     stateLogEle.innerHTML += `<p>${logText}</p>`;
 
@@ -30,16 +30,16 @@ class SelectView {
   }
 
   renderStateLog({ targetValue, productName, type }) {
-    let logText = "";
-    if (type === "wallet") logText = `${targetValue}원이 투입되었습니다.`;
-    else if (type === "dial") logText = `${productName}이(가) 팔렸습니다.`;
-    else if (type === "err") logText = `잔액이 부족합니다`;
+    let logText = '';
+    if (type === 'wallet') logText = `${targetValue}원이 투입되었습니다.`;
+    else if (type === 'dial') logText = `${productName}이(가) 나왔습니다.`;
+    else if (type === 'err') logText = `잔액이 부족합니다`;
 
     return logText;
   }
 
   renderAccumulateInput(value) {
-    const inputAmountEle = this.element.querySelector(".choice_product_price");
+    const inputAmountEle = this.element.querySelector('.choice_product_price');
     inputAmountEle.innerText = value;
   }
 
