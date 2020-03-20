de942efc3cdce16e877dfce9d1d94d1b5bb31522
최장호 <hmchoi651@naver.com>
2020-03-20 15:06:20 +0900

    [추가] VendingMachineModel 잔돈 반환 로직 추가
    <VendingMachineModel >
    state :                            change-( productView에 전달될 시 true 라면 productView를 초기화 시켜주는 값 )
                                          type-(select View에 type이 timer로 넘어가는 추가사항 구현)
    
    addEventDial method : switch문으로 변경
                                         number type click 시 clearTimeout 추가
                                         select type click 시 accumulatedAmount 값에 따라서 return 하는 로직 구현 && 잔돈 반환시키는 로직 구현
    
    resetVMModel method : select 시 selectView , productView에 필요한 default 값 설정

--- src/model/VendingMachineModel.js
+++ src/model/VendingMachineModel.js
@@ -1,15 +1,19 @@
-import Observable from './observable.js';
+import Observable from "./observable.js";
 
 class VendingMachineModel extends Observable {
-  constructor() {
+  constructor(timerModel) {
     super();
+
+    this.timerModel = timerModel;
+
     this.state = {
       productData: null,
       accumulatedAmount: 0,
       targetValue: null,
-      productNumber: '',
-      productName: '',
-      type: ''
+      productNumber: "",
+      productName: "",
+      type: "",
+      change: false
     };
   }
 
@@ -21,33 +25,61 @@ class VendingMachineModel extends Observable {
     await this.notify(this.state);
   }
 
+  resetVMModel() {
+    this.state.accumulatedAmount = 0;
+    this.state.type = "timer";
+    this.state.change = true;
+    this.notify(this.state);
+    this.state.change = false;
+  }
+
   addEventWallet(targetCountNumber) {
     const target = event.target;
     const targetCount = Number(targetCountNumber);
     if (!targetCount) return;
     this.state.targetValue = target.value;
     this.state.accumulatedAmount += Number(target.value);
-    this.state.type = 'wallet';
+    this.state.type = "wallet";
     this.filterActiveProduct(this.state.productData);
     this.notify(this.state);
   }
 
   addEventDial(dial, type) {
-    if (type === 'number') this.state.productNumber += dial;
-    else if (type === 'select') this.clickSelectDial(this.state.productNumber);
-    else if (type === 'cancel') this.state.productNumber = '';
+    switch (type) {
+      case "number":
+        clearTimeout(this.timerModel.state.timer);
+        this.state.productNumber += dial;
+        break;
+      case "select":
+        this.clickSelectDial(this.state.productNumber);
+        if (this.state.accumulatedAmount === 0) {
+          alert(`누적된 금액이 0원 입니다. 금액을 투입해 주세요`);
+          return;
+        }
+
+        this.timerModel.state.accumulatedAmount = this.state.accumulatedAmount;
+        this.timerModel.state.timer = setTimeout(() => {
+          this.timerModel.addEvent();
+          this.resetVMModel();
+        }, 5000);
+
+        break;
+      case "cancel":
+        this.state.productNumber = "";
+        break;
+    }
   }
 
   clickSelectDial(number) {
     const productId = Number(number);
     if (!productId || productId > this.state.productData.length)
-      return (this.state.productNumber = '');
-    this.state.type = 'dial';
+      return (this.state.productNumber = "");
+    this.state.type = "dial";
     const filteringProduct = this.filterIdProduct(productId);
 
     if (this.state.accumulatedAmount < Number(filteringProduct.price)) {
-      this.state.productNumber = '';
-      this.state.type = 'err';
+      this.state.productNumber = "";
+      this.state.type = "err";
       return this.notify(this.state);
     }
 
@@ -56,7 +88,7 @@ class VendingMachineModel extends Observable {
     this.filterActiveProduct(this.state.productData);
     this.notify(this.state);
 
-    this.state.productNumber = '';
+    this.state.productNumber = "";
   }
 
   filterIdProduct(id) {
